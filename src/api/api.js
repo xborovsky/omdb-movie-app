f027df288079924f1dd5b9c2af0b8c52bd0b89a7
Marek Borovsky <borovsky@oksystem.cz>
2018-08-02 11:25:22 +0200

    Use heroku cors-anywhere, remove unused file

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API_KEY = 'd99bf8d8';
-const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+const BASE_URL = `http://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?apikey=${API_KEY}`;
 
 export const searchMovies = (searchText, page = 1) => {
     return axios.get(`${BASE_URL}&s=${searchText}&r=json&page=${page}&type=movie`)
--- src/api/poster-api.js
+++
@@ -1,3 +0,0 @@
-import {API_KEY} from './api';
-
-const BASE_URL = `http://img.omdbapi.com/?apikey=${api}&`;
